0433d054de0e7e477a4e9eaf02819c30109b0379
MaximoFN <maximofn@gmail.com>
2023-03-31 08:02:35 +0200

    Primera versión del chat. Escriba lo que escriba siempre responde "hola chat"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+def main():
+    st.title("Simple Chat App")
+    st.write("Escribe un mensaje y la respuesta será siempre 'hola chat'.")
+
+    user_input = st.text_input("Escribe tu mensaje aquí:")
+    if user_input:
+        st.write("Respuesta: hola chat")
+
+if __name__ == "__main__":
+    main()
 
